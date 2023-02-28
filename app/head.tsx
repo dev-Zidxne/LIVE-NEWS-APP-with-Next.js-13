8f77587e3188164b6c5e36c2a36dba366c399edb
Zidane Innis <zidaneinnis@gmail.com>
2023-02-28 17:51:48 -0500

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Head() {
+  return (
+    <>
+      <meta content="width-device-width,initial-scale-1"></meta>
+      <title>GOTHAM NEWS</title>
+      <link rel="icon" href="/favicon./ico"></link>
+    </>
+  );
+}
+
+export default Head;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import "../styles/globals.css";
+
+function Homepage() {
+  return <div>Homepage</div>;
+}
+
+export default Homepage;
