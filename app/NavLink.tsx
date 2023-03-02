e689b79f66fd2fb3bb3fbb63296ebeb1555ea20e
Zidane Innis <zidaneinnis@gmail.com>
2023-03-02 12:52:13 -0500

    Building NavLinks done.

--- app/NavLink.tsx
+++ app/NavLink.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import Link from "next/link";
 
 type Props = {
   category: string;
   isActive: boolean;
 };
 
-function NavLink({ category }: Props) {
-  return <div>NavLink</div>;
+function NavLink({ category, isActive }: Props) {
+  return (
+    <Link
+      href={`/news/${category}`}
+      className={`navLink
+        ${
+          isActive &&
+          "underline decoration-orange-400 underline-offset-4 font-bold text-lg"
+        }`}
+    >
+      {category}
+    </Link>
+  );
 }
 
 export default NavLink;
--- app/NavLinks.tsx
+++ app/NavLinks.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import { categories } from "../constant";
 import NavLink from "./NavLink";
+import { usePathname } from "next/navigation";
 
 function NavLinks() {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => {
+    return pathname?.split("/").pop() === path;
+  };
   return (
-    <nav>
+    <nav className="grid max-w-6xl grid-cols-4 gap-4 pb-10 mx-auto text-xs border-b md:grid-cols-7 md:text-sm">
       {categories.map((category) => (
-        <NavLink key={category} category={category} />
+        <NavLink
+          key={category}
+          category={category}
+          isActive={isActive(category)}
+        />
       ))}
     </nav>
   );
